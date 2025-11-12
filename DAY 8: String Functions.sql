042df83e133e6bd1a6abd78f5ad75ecbaf604294
Aditto Ahosan Kabbo <aditahosankabbo@gmail.com>
2025-11-12 11:21:09 +0600

    Create DAY 8: String Functions.sql

---
+++ DAY 8: String Functions.sql
@@ -0,0 +1,19 @@
+-- =============================================
+-- DAY 8: Exploring String Functions
+-- =============================================
+-- Question 1: Create a patient summary that shows: patient_id, full name in uppercase, service in lowercase, age category (if age >= 65 then 'Senior', if age >= 18 then 'Adult', else 'Minor'), and name length. Only show patients whose name length is greater than 10 characters.
+
+SELECT
+    patient_id, 
+    UPPER(name) AS full_name, 
+    LOWER(service) AS service, 
+    CASE 
+        WHEN age >= 65 THEN 'Senior' 
+        WHEN age >= 18 THEN 'Adult'
+        ELSE 'Minor'
+    END AS age_category,
+    LEN(name) AS name_length 
+FROM
+    patients 
+WHERE 
+    LEN(name) > 10;
