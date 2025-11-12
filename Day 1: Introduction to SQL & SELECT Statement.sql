2f390cea784ef812ccb219896ac8bcb16295ee9f
Aditto Ahosan Kabbo <aditahosankabbo@gmail.com>
2025-11-12 11:01:06 +0600

    Add SQL query to list unique hospital services

---
+++ Day 1: Introduction to SQL & SELECT Statement.sql
@@ -0,0 +1,8 @@
+-- =============================================
+-- DAY 1: INTRODUCTION TO SQL & SELECT STATEMENT
+-- =============================================
+
+-- Question 1: List all unique hospital services available in the hospital.
+
+SELECT DISTINCT service
+FROM services_weekly;
